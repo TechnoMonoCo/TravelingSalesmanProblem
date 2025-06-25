6448105d6379d632391dcfab09f0c34af39e784c
Nick <steichennicholas@gmail.com>
2025-06-25 18:29:29 -0500

    Implement Nearest Neighbor (#2)
    Nearest Neighbor will be the baseline we use to compare the new algorithm against.

---
+++ TSP_Add_Shortest/solvers/NearestNeighbor.cs
@@ -0,0 +1,75 @@
+﻿using TSP_Add_Shortest.objects;
+
+namespace TSP_Add_Shortest.solvers
+{
+    public class NearestNeighbor
+    {
+        public readonly List<Node> nodes;
+        private readonly List<Node> traversalOrder = [];
+
+        public NearestNeighbor(List<Node> nodes)
+        {
+            if (nodes.Count == 0)
+            {
+                throw new Exception("Nodes must be a non-empty list.");
+            }
+
+            this.nodes = nodes;
+        }
+
+        public void Solve()
+        {
+            if (traversalOrder.Count != 0)
+            {
+                return;
+            }
+
+            var visited = Enumerable.Repeat(false, nodes.Count).ToList();
+
+            var lastVisited = nodes.First();
+            traversalOrder.Add(lastVisited);
+            visited[0] = true;
+
+            while (traversalOrder.Count < nodes.Count)
+            {
+                Node? best = null;
+                var bestDistance = double.MaxValue;
+                var loc = -1;
+                for (var i = 0; i < nodes.Count; i++)
+                {
+                    if (visited[i])
+                    {
+                        continue;
+                    }
+
+                    if (best == null)
+                    {
+                        best = nodes[i];
+                        bestDistance = lastVisited.Distance(best);
+                        loc = i;
+                    } else
+                    {
+                        var testDistance = lastVisited.Distance(nodes[i]);
+                        if (testDistance < bestDistance)
+                        {
+                            bestDistance = testDistance;
+                            best = nodes[i];
+                            loc = i;
+                        }
+                    }
+                }
+
+                lastVisited = best;
+                traversalOrder.Add(best);
+                visited[loc] = true;
+            }
+            // Add the final connection point in.
+            traversalOrder.Add(nodes[0]);
+        }
+
+        public List<Node> GetPath()
+        {
+            return traversalOrder;
+        }
+    }
+}
---
+++ TSP_Add_Shortest_Tests/solvers/TestNearestNeighbor.cs
@@ -0,0 +1,70 @@
+﻿using TSP_Add_Shortest.objects;
+using TSP_Add_Shortest.solvers;
+
+namespace TSP_Add_Shortest_Tests.solvers
+{
+    [TestClass]
+    public class TestNearestNeighbor
+    {
+        private static void RunTest(List<Node> nodes)
+        {
+            var nearestNeighbor = new NearestNeighbor(nodes);
+            nearestNeighbor.Solve();
+            var route = nearestNeighbor.GetPath();
+
+            Assert.AreEqual(nodes.Count + 1, route.Count);
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                Assert.AreEqual(nodes[i].id, route[i].id);
+            }
+            Assert.AreEqual(nodes.First().id, route.Last().id);
+        }
+
+        [TestMethod]
+        public void TestKnownLongRoute()
+        {
+            var nodes = new List<Node> {
+                new(0, 0),
+                new(0, 1),
+                new(0, 2),
+                new(1, 2),
+                new(2, 2),
+                new(2, 1),
+                new(2, 0),
+                new(1, 0),
+            };
+
+            RunTest(nodes);
+        }
+
+        [TestMethod]
+        public void TestKnownShortRoute()
+        {
+            var nodes = new List<Node> {
+                new(0, 0),
+                new(0, 1),
+            };
+
+            RunTest(nodes);
+        }
+
+
+        [TestMethod]
+        public void TestKnownSingleItemRoute()
+        {
+            var nodes = new List<Node> {
+                new(0, 0),
+            };
+
+            RunTest(nodes);
+        }
+
+        [TestMethod]
+        public void TestThrowsWhenEmptyNodes()
+        {
+            var exception = Assert.ThrowsException<Exception>(
+                () => new NearestNeighbor([])
+            );
+        }
+    }
+}
