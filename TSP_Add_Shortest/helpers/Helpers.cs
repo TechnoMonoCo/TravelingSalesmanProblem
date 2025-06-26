77597108ed24d1a7a4727c7f67048205df55935b
Nick <steichennicholas@gmail.com>
2025-06-25 19:07:04 -0500

    Implement path distance calculator
    A generic helper that will be used to calculate the final distance of generated paths.

---
+++ TSP_Add_Shortest/helpers/Helpers.cs
@@ -0,0 +1,24 @@
+﻿using TSP_Add_Shortest.objects;
+
+namespace TSP_Add_Shortest.helpers
+{
+    public static class Helpers
+    {
+        public static double CalculatePathDistance(List<Node> nodes)
+        {
+            if (nodes == null || nodes.Count == 0)
+            {
+                return 0;
+            }
+
+            var lastNode = nodes.First();
+            double distance = 0;
+            foreach (var node in nodes)
+            {
+                distance += lastNode.Distance(node);
+                lastNode = node;
+            }
+            return distance;
+        }
+    }
+}
---
+++ TSP_Add_Shortest_Tests/helpers/TestHelpers.cs
@@ -0,0 +1,64 @@
+﻿using TSP_Add_Shortest.helpers;
+using TSP_Add_Shortest.objects;
+
+namespace TSP_Add_Shortest_Tests.helpers
+{
+    [TestClass]
+    public class TestHelpers
+    {
+        [TestMethod]
+        public void TestCalculateDistanceSimplePath()
+        {
+            var nodes = new List<Node>
+            {
+                new(0,0),
+                new(0,1),
+                new(1,1),
+                new(1,0),
+                new(0,0),
+            };
+
+            Assert.AreEqual(4, Helpers.CalculatePathDistance(nodes));
+        }
+
+        [TestMethod]
+        public void TestCalculateDistanceBasicPath()
+        {
+            var nodes = new List<Node>
+            {
+                new(0,0),
+                new(0,1),
+                new(1,1),
+                new(1,0),
+                new(2,0),
+                new(2,1),
+            };
+            Assert.AreEqual(5, Helpers.CalculatePathDistance(nodes));
+        }
+
+        [TestMethod]
+        public void TestCalculateDistanceComplexPath()
+        {
+            var nodes = new List<Node>
+            {
+                new(0,0),
+                new(3,0),
+                new(3,4),
+                new(0,0),
+            };
+            Assert.AreEqual(12, Helpers.CalculatePathDistance(nodes));
+        }
+
+        [TestMethod]
+        public void TestCalculateDistanceEmptyPath()
+        {
+            Assert.AreEqual(0, Helpers.CalculatePathDistance([]));
+        }
+
+        [TestMethod]
+        public void TestCalculateDistanceNullPath()
+        {
+            Assert.AreEqual(0, Helpers.CalculatePathDistance(null));
+        }
+    }
+}
