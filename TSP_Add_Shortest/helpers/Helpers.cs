47360f5411254537c34e9e70ddd07fe5283a67f3
Nick <steichennicholas@gmail.com>
2025-06-28 11:34:49 -0500

    Implements helper to generate Add Shortest path from connected nodes
    Because we are using the nodes themselves to control if we can connect or not, we need to translate this to a list of nodes to be more readily understood by other aspects of the code.

--- TSP_Add_Shortest/helpers/Helpers.cs
+++ TSP_Add_Shortest/helpers/Helpers.cs
@@ -25,5 +25,82 @@ namespace TSP_Add_Shortest.helpers
             }
             return distance;
         }
+
+        /// <summary>
+        /// Traverses the path of a connected list of nodes, returning the order.
+        /// </summary>
+        /// <param name="nodes"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception">In any case where the route cannot be traversed</exception>
+        public static List<Node> ConnectedNodesToPath(List<Node> nodes)
+        {
+            Node? startPoint = null;
+            Node? endPoint = null;
+
+            foreach(var node in nodes)
+            {
+                if (node.IsFullyConnected())
+                {
+                    continue;
+                }
+
+                if (node.left == null && node.right == null)
+                {
+                    throw new Exception("Expected all nodes provided to be connected but weren't.");
+                }
+
+                if (startPoint == null)
+                {
+                    startPoint = node;
+                } else if (endPoint == null)
+                {
+                    endPoint = node;
+                } else
+                {
+                    throw new Exception("At least 3 nodes were detected as endpoints, expected exactly 2.");
+                }
+            }
+
+            if (startPoint == null)
+            {
+                throw new Exception("No endpoint detected - cannot determine path.");
+            }
+
+            // Can only happen if there is a single endpoint element in the list.
+            // Only happens if one of the endpoints is not provided in the list.
+            if (endPoint == null)
+            {
+                throw new Exception("Only one endpoint detected - cannot determine path.");
+            }
+
+            int previousId = startPoint.id;
+            var path = new List<Node> { startPoint };
+            var root = startPoint.right != null ? startPoint.right : startPoint.left;
+            while (root.id != endPoint.id) {
+                path.Add(root);
+                var currentId = root.id;
+
+                // While the right and left of the root shouldn't be null,
+                // double check them just to be safe.
+                if (root.right != null && root.right.id != previousId)
+                {
+                    root = root.right;
+                }
+                if (root.left != null && root.left.id != previousId)
+                {
+                    root = root.left;
+                }
+
+                // This should not be possible, would require you to be looped on self.
+                if (root.id == currentId)
+                {
+                    throw new Exception("Stuck on a node and cannot traverse path.");
+                }
+                previousId = currentId;
+            }
+            path.Add(root);
+            path.Add(path[0]);
+            return path;
+        }
     }
 }
