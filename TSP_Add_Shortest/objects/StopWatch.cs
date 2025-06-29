648768a0225b3283d3a0a1ab25969561a5f02349
Nick <steichennicholas@gmail.com>
2025-06-29 07:48:52 -0500

    Implements StopWatch
    The stopwatch will be used to indicate runtimes for NN and AS.

---
+++ TSP_Add_Shortest/objects/StopWatch.cs
@@ -0,0 +1,67 @@
+﻿namespace TSP_Add_Shortest.objects
+{
+    public class StopWatch
+    {
+        public DateTime? startTime { get; private set; } = null;
+        public DateTime? endTime { get; private set; } = null;
+
+        /// <summary>
+        /// Starts the stop watch
+        /// </summary>
+        /// <exception cref="Exception">If the stopwatch is currently running.</exception>
+        public void Start()
+        {
+            if (startTime != null)
+            {
+                throw new Exception("Cannot start an already started stopwatch.");
+            }
+            startTime = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Stops an already running stopwatch.
+        /// </summary>
+        /// <exception cref="Exception">If the stopwatch has not been started or was already stopped.</exception>
+        public void Stop()
+        {
+            if (endTime != null)
+            {
+                throw new Exception("Cannot stop an already stopped stopwatch.");
+            }
+            if (startTime == null)
+            {
+                throw new Exception("Stopwatch must be started first.");
+            }
+            endTime = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Returns the total time between start and stop in ms.
+        /// </summary>
+        /// <returns>Stopwatch duration in ms</returns>
+        /// <exception cref="Exception">If the stopwatch has not been start, or has been started but not stopped.</exception>
+        public int DurationInMs()
+        {
+            if (startTime == null)
+            {
+                throw new Exception("Stopwatch was never started.");
+            }
+            if (endTime == null)
+            {
+                throw new Exception("Stopwatch was never stopped.");
+            }
+
+            var diff = endTime.Value - startTime.Value;
+            return (int)diff.TotalMilliseconds;
+        }
+
+        /// <summary>
+        /// Resets the stopwatch to default for another use.
+        /// </summary>
+        public void Reset()
+        {
+            startTime = null;
+            endTime = null;
+        }
+    }
+}
---
+++ TSP_Add_Shortest_Tests/objects/TestStopWatch.cs
@@ -0,0 +1,80 @@
+﻿using TSP_Add_Shortest.objects;
+
+namespace TSP_Add_Shortest_Tests.objects
+{
+    [TestClass]
+    public class TestStopWatch
+    {
+        [TestMethod]
+        public void Test_Reset_WhenAlreadyReset()
+        {
+            var stopwatch = new StopWatch();
+            Assert.IsNull(stopwatch.startTime);
+            Assert.IsNull(stopwatch.endTime);
+            stopwatch.Reset();
+            Assert.IsNull(stopwatch.startTime);
+            Assert.IsNull(stopwatch.endTime);
+        }
+
+        [TestMethod]
+        public void Test_Reset_Resets()
+        {
+            var stopwatch = new StopWatch();
+            stopwatch.Start();
+            stopwatch.Stop();
+            Assert.IsNotNull(stopwatch.startTime);
+            Assert.IsNotNull(stopwatch.endTime);
+            stopwatch.Reset();
+            Assert.IsNull(stopwatch.startTime);
+            Assert.IsNull(stopwatch.endTime);
+        }
+
+        [TestMethod]
+        public void Test_Start_ThrowsWhenAlreadyStarted()
+        {
+            var stopwatch = new StopWatch();
+            stopwatch.Start();
+            Assert.IsNotNull(stopwatch.startTime);
+            var exception = Assert.ThrowsException<Exception>(
+                () => stopwatch.Start()
+            );
+            Assert.AreEqual("Cannot start an already started stopwatch.", exception.Message);
+        }
+
+        [TestMethod]
+        public void Test_Stop_ThrowsWhenNotStarted()
+        {
+            var stopwatch = new StopWatch();
+            Assert.IsNull(stopwatch.startTime);
+            var exception = Assert.ThrowsException<Exception>(
+                () => stopwatch.Stop()
+            );
+            Assert.AreEqual("Stopwatch must be started first.", exception.Message);
+        }
+
+        [TestMethod]
+        public void Test_Stop_ThrowsWhenAlreadyStopped()
+        {
+            var stopwatch = new StopWatch();
+            stopwatch.Start();
+            stopwatch.Stop();
+            Assert.IsNotNull(stopwatch.startTime);
+            Assert.IsNotNull(stopwatch.endTime);
+            var exception = Assert.ThrowsException<Exception>(
+                () => stopwatch.Stop()
+            );
+            Assert.AreEqual("Cannot stop an already stopped stopwatch.", exception.Message);
+        }
+
+        [TestMethod]
+        public void Test_DurationInMs_ReturnsAPossitiveValue()
+        {
+            var stopwatch = new StopWatch();
+            stopwatch.Start();
+            stopwatch.Stop();
+            Assert.IsNotNull(stopwatch.startTime);
+            Assert.IsNotNull(stopwatch.endTime);
+            Assert.IsTrue(stopwatch.DurationInMs() >= 0);
+        }
+    }
+}
