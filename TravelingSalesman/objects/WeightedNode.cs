335a23191a14188b2ed5aee32f97382b677309b3
Nick <steichennicholas@gmail.com>
2025-07-09 13:49:38 -0500

    Adds WeightedNode class (#17)
    Also fixes a bug in project references.

---
+++ TravelingSalesman/objects/WeightedNode.cs
@@ -0,0 +1,7 @@
+﻿namespace TravelingSalesman.objects
+{
+    public class WeightedNode(double x, double y) : Node(x, y)
+    {
+        public double Weight { get; set; } = 0;
+    }
+}
---
+++ TravelingSalesmanTests/objects/TestWeightedNode.cs
@@ -0,0 +1,17 @@
+﻿using TravelingSalesman.objects;
+
+namespace TravelingSalesmanTests.objects
+{
+    [TestClass]
+    public class TestWeightedNode
+    {
+        [TestMethod]
+        public void Test_GetAndSetWorkAsExpected()
+        {
+            var weightedNode = new WeightedNode(1, 2);
+            Assert.AreEqual(0, weightedNode.Weight);
+            weightedNode.Weight = 5.3;
+            Assert.AreEqual(5.3, weightedNode.Weight);
+        }
+    }
+}
