a100a1ea2298f0a70b91ca6b102ffb15c0d0b713
Nick <steichennicholas@gmail.com>
2025-07-10 14:25:42 -0500

    Adds WNN to Runner

--- TravelingSalesman/Program.cs
+++ TravelingSalesman/Program.cs
@@ -11,9 +11,13 @@ var asWins = 0;
 double asTime = 0;
 double asDistance = 0;
 
+var wnnWins = 0;
+double wnnTime = 0;
+double wnnDistance = 0;
+
 var ties = 0;
 
-for (var i = 5; i < 1000; i += 5)
+for (var i = 10; i <= 1000; i += 10)
 {
     Console.WriteLine($"\n\n\nSTARTING RUN WITH {i} NODES");
     var runner = new Runner(i, 100, false);
@@ -28,6 +32,10 @@ for (var i = 5; i < 1000; i += 5)
     asTime += runner.asTotalDuration;
     asDistance += runner.asTotalDistance;
 
+    wnnWins += runner.wnnWins;
+    wnnTime += runner.wnnTotalDuration;
+    wnnDistance += runner.wnnTotalDistance;
+
     ties += runner.ties;
 }
 
@@ -36,4 +44,5 @@ var runs = asWins + nnWins + ties;
 Console.WriteLine("\n\n\n\n\nFINAL STATS!\n---------");
 Console.WriteLine($"NN:\nWINS:{nnWins} ({100.0 * nnWins / runs})\nTIME: {nnTime}ms\nDIST: {nnDistance}\n");
 Console.WriteLine($"AS:\nWINS:{asWins} ({100.0 * asWins / runs})\nTIME: {asTime}ms\nDIST: {asDistance}");
+Console.WriteLine($"WNN:\nWINS:{wnnWins} ({100.0 * wnnWins / runs})\nTIME: {wnnTime}ms\nDIST: {wnnDistance}\n");
 Console.WriteLine($"Ties: {ties} ({100.0 * ties / runs})");
